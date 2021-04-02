17646edc0002e1af306b8bb4eeaf6dc48409ba95
Dzheremilz <trempejeremie@gmail.com>
2021-04-02 16:45:04 +0200

    update

--- main.js
+++ main.js
@@ -3,22 +3,35 @@ const readlineSync = require('readline-sync')
 const { hangingState } = require('./hangmanAscii')
 
 const word = randomWord('hangman.txt')
-console.log(word)
+//console.log(word)
 
 let isSearching = true
 let hiddingWord = '_ '.repeat(word.length)
+let guessArray = []
 let nbTry = 7
 let hangState = 0
 let score = 0
 
 
 while (isSearching) {
-  console.log('Try to guess: ', hiddingWord)
+  console.log('Try to guess: ', hiddingWord, guessArray.length > 0 ? `\nAlready use: ${guessArray.join(' ')}` : '')
   const answer = readlineSync.question('Choose one letter or guess the word: ').toLowerCase()
   score++
+  /*
+  if (word === answer) {
+    console.log('CONGRATURATION !!!')
+    break
+  }
+  */
   if (answer.length !== 1 || (answer.charCodeAt(0) < 97 || answer.charCodeAt(0) > 122)) {
-    console.log('Error: Choose one letter')
+    console.log('Error: Choose one letter or guess the word\n')
     continue
+  }
+  if (guessArray.includes(answer)) {
+    console.log(`Vous avez déja essayé avec ${answer}`)
+    continue
+  } else {
+    guessArray.push(answer)
   }
   if (word.includes(answer)) {
     for (let i = 0; i < word.length; i++) {
@@ -37,10 +50,17 @@ while (isSearching) {
     isSearching = false
   }
   if (hangState === nbTry) {
-    console.log('Game Over')
+    console.log(`Game Over\nLe mot à trouver était: ${word}`)
+    score = 0
     isSearching = false
   }
 }
 
 console.log(`Your score: ${score}`)
-
+/*
+if (score > 0) {
+  const player = readlineSync.question('Enter your name: ')
+  // compare score to highscore
+  //newScore(player, score)
+}
+*/
